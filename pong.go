a260692bcda20ac34988710d0cfbfa7f453aaf33
Jon Daniel <binarycleric@gmail.com>
2016-12-22 22:22:01 -0500

    Initial commit

---
+++ pong.go
@@ -0,0 +1,33 @@
+package main
+
+import (
+	"fmt"
+	// "io/ioutil"
+	"log"
+	"net/http"
+	"os"
+	"time"
+)
+
+func pong(res http.ResponseWriter, req *http.Request) {
+	fmt.Println(time.Now().Format(time.RFC3339) + " :: pong")
+	fmt.Fprintln(res, "pong")
+}
+
+func main() {
+	port := os.Getenv("PORT")
+
+	if port == "" {
+		port = "8080"
+	}
+
+	log.SetOutput(os.Stdout)
+	http.HandleFunc("/", pong)
+	fmt.Println("listening...")
+
+	err := http.ListenAndServe(":"+port, nil)
+	if err != nil {
+		panic(err)
+	}
+
+}
